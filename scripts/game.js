9922e1363c6135bbc5cfbe372a92fb88a08e09ff
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2017-12-09 00:12:04 -0600

    Add files via upload

--- scripts/game.js
+++ scripts/game.js
@@ -1,16 +1,20 @@
-// Listen for Keypress
-
-document.addEventListener("keypress", getKey);
+/* document.addEventListener("keypress", getKey);
     function getKey(ev) {
         console.log(ev.key);
-    }
+    } */
 
 
-// This function will pick our word
-var category = ["HTML", "Javascript", "Closure", "Event"];
-function chooseWord(category) {
-    return category[Math.floor(Math.random() * category.length)];
-  
+// ARRAYS & VARIABLES
+var words = ["html", "javascript", "closure", "event"]; // LIST OF WORDS
+var word = words[Math.floor(Math.random() * words.length)]; // NEW VARIABLE 'WORD': This new var = a random selection 
+
+var answerArray = []; // Empty Array to be filled with underscores to match letters in word
+for (var i = 0; i < word.length; i++) { // 
+    answerArray[i] = "_";
 }
 
-
+var remainingLetters = word.length; // Create the variable 'remainingLetters' and set it to the length of the secret word.
+console.log(word);
+console.log(remainingLetters);
+console.log(answerArray);
+(answerArray.join(" "));
