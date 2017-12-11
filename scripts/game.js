37a6f9aa68ac0976df66192af68ba9ddd63e32c9
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2017-12-10 19:43:27 -0600

    Add files via upload

--- scripts/game.js
+++ scripts/game.js
@@ -4,17 +4,28 @@
     } */
 
 
-// ARRAYS & VARIABLES
+// ARRAYS 
 var words = ["html", "javascript", "closure", "event"]; // LIST OF WORDS
 var word = words[Math.floor(Math.random() * words.length)]; // NEW VARIABLE 'WORD': This new var = a random selection 
 
-var answerArray = []; // Empty Array to be filled with underscores to match letters in word
-for (var i = 0; i < word.length; i++) { // 
+// Use an array of blanks for each letter in the word. This the ANSWER ARRAY, and we’ll fill it with the player’s CORRECT GUESSES as they’re made.
+// The answer array will start out as a group of these empty entries equal in number to the letters in the secret word.
+
+// ARRAY OF BLANKS. Example: HTML (randomly Selected) "_ " + "_ " "_ " + "_ "
+var answerArray = []; 
+
+// Creates a looping variable i that starts at 0 and goes up to (but does not include) word.length.
+// Each time around the loop, we add a new element to answerArray, at answerArray[i].When the loop finishes, 
+// answerArray will be the same length as word. 
+for (var i = 0; i < word.length; i++) { 
     answerArray[i] = "_";
 }
-
-var remainingLetters = word.length; // Create the variable 'remainingLetters' and set it to the length of the secret word.
+// Create the variable 'remainingLetters' and set it to the length of the secret word.
+var remainingLetters = word.length; 
+document.getElementById("wordDisplay").innerHTML = answerArray.join(" ");
+// Console Logs
 console.log(word);
 console.log(remainingLetters);
 console.log(answerArray);
-(answerArray.join(" "));
+
+// document.getElementById("wordDisplay").innerHTML = answerArray; 
